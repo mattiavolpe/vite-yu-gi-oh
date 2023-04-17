aee5b27993f9eba716ef7e1742904f460e0dc5cf
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-17 18:30:19 +0200

    FIX background on header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,8 +5,8 @@ export default {
 </script>
 
 <template>
-<header>
-  <div class="container-xxl p-3 d-flex align-items-center justify-content-center flex-wrap bg-light text-dark">
+<header class="bg-light text-dark">
+  <div class="container-xxl p-3 d-flex align-items-center justify-content-center flex-wrap">
     <img width="150" src="./../assets/img/Yu-Gi-Oh_Logo.webp" alt="Yu-Gi-Oh logo">
     <h1 class="m-0 ms-3">Yu-Gi-Oh Cards</h1>
   </div>
