0af14876240498d6f46ba94e1b76cda9fe26fcdc
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-17 16:54:42 +0200

    ADD CardItem

---
+++ src/components/CardItem.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: "CardItem",
+  props: {
+    card: Object,
+  }
+}
+</script>
+
+<template>
+  <div class="cardItem col text-center">
+    <div class="card">
+      <img class="img-fluid" :src="card.card_images[0].image_url" :alt="card.name + ' image'">
+      <h4 class="my-2">{{ card.name }}</h4>
+      <span>{{ card.type }}</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@use "./../assets/scss/app.scss" as *;
+
+.card {
+  background-color: $primary !important;
+  height: 100% !important;
+}
+</style>
