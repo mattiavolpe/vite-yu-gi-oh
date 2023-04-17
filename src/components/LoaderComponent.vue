8a4f23530d8a890455f35836b217a42b99355bd4
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-17 18:31:28 +0200

    ADD background to loader component

--- src/components/LoaderComponent.vue
+++ src/components/LoaderComponent.vue
@@ -5,7 +5,7 @@ export default {
 </script>
 
 <template>
-  <div class="text-center">
+  <div class="text-center bg-light text-dark">
     <img class="w-25" src="./../assets/img/Yu-Gi-Oh_Logo.webp" alt="">
     <h2 class="my-3">Loading content...</h2>
   </div>
